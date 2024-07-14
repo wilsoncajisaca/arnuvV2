5e3d467ab34f65633e2263c500ec4926cd373bcb
wcajisaca <wcajisaca@ec.krugercorp.com>
2024-07-14 15:53:12 -0500

    Se procede a agregar las plantillas de login

--- src/main/java/com/core/arnuv/controller/IndexController.java
+++ src/main/java/com/core/arnuv/controller/IndexController.java
@@ -1,14 +1,7 @@
 package com.core.arnuv.controller;
 
-import com.core.arnuv.model.Usuariodetalle;
-import com.core.arnuv.service.IPersonaDetalleService;
-import com.core.arnuv.service.IUsuarioDetalleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -16,22 +9,5 @@ public class IndexController {
     @GetMapping("/index")
     public String index() {
         return "/index";
-    }
-
-    @Autowired
-    private IUsuarioDetalleService servicioUsuarioDetalle;
-
-    @GetMapping("/listarusuario")
-    public String listarUsuarios(Model model) {
-        List<Usuariodetalle> lusuariodetalle = servicioUsuarioDetalle.listarTodosUsuariosDetalle();
-        model.addAttribute("listaUserD", lusuariodetalle);
-        return "/usuario/listarusuario";
-    }
-
-    @GetMapping("/nuevousuario")
-    public String nuevosClientes(Model model) {
-//        List<Usuariodetalle> lusuariodetalle = servicioUsuarioDetalle.listarTodosUsuariosDetalle();
-//        model.addAttribute("listaUserD", lusuariodetalle);
-        return "/usuario/nuevousuario";
     }
 }
