9dadff8fd244580bdf2823ee581b5ad1d8ee8329
wcajisaca <wcajisaca@ec.krugercorp.com>
2024-08-12 18:49:03 -0500

    Se agregan handlers de excepciones

--- src/main/java/com/core/arnuv/handlers/WebExceptionHandler.java
+++ src/main/java/com/core/arnuv/handlers/WebExceptionHandler.java
@@ -3,17 +3,26 @@ package com.core.arnuv.handlers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class CustomExceptionHandler {
+public class WebExceptionHandler {
     @ExceptionHandler(AccessDeniedException.class)
     public String handleAccessDeniedException(AccessDeniedException ex, Model model) {
         model.addAttribute("status", 403);
         model.addAttribute("message", "Access is denied");
         model.addAttribute("timestamp", LocalDateTime.now());
-        return "landing/access-denied"; // nombre de la vista Thymeleaf
+        return "landing/access-denied";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handleNotFoundException(NoHandlerFoundException ex, Model model) {
+        model.addAttribute("status", 404);
+        model.addAttribute("message", "Page not found");
+        model.addAttribute("timestamp", LocalDateTime.now());
+        return "landing/access-denied";
     }
 }
